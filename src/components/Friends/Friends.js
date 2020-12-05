d2b8c73f19fbb12fe89b6035e6d1436c969f1233
VikMelanchenko <63492650+VikMelanchenko@users.noreply.github.com>
2020-12-05 21:34:08 +0200

    fixed status isOnLine

--- src/components/Friends/Friends.js
+++ src/components/Friends/Friends.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import PropTypes from 'prop-types';
+
 import FriendListItem from './Friend';
 
 import s from './Friends.module.scss';
@@ -7,16 +9,20 @@ import s from './Friends.module.scss';
 const FriendList = ({ friends }) => {
   return (
     <ul className={s.friendList}>
-      {friends.map(({ id, avatar, name, isOnline }) => (
-        <FriendListItem
-          key={id}
-          avatar={avatar}
-          name={name}
-          isOnline={isOnline}
-        />
-      ))}
+      <FriendListItem friends={friends} />
     </ul>
   );
+};
+
+FriendListItem.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
 };
 
 export default FriendList;
